c7d6190a332afa4e7dd2cb3a57804d5cc3b8206c
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-02-11 18:24:35 +0300

    добавила фикстуру и рабочий тест

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selene import browser, be, have
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def browser_config():
     browser.config.window_height = 2500
     browser.config.window_width = 1400
---
+++ test_hw.py
@@ -0,0 +1,11 @@
+from selene import browser, be, have
+
+def test_google_search_(browser_config):
+    browser.open('https://duckduckgo.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('html').should(have.text('Selene: User-Oriented Web UI Browser Tests in Python'))
+
+def test_google_search_unsuccessful(browser_config):
+    browser.open('https://duckduckgo.com')
+    browser.element('[name="q"]').should(be.blank).type('автокпмифмдфцлшомт ырв334').press_enter()
+    browser.element('html').should(have.text('результаты не найдены'))
