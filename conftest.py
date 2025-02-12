1ad4037a64eb7c1bd74642fa783c530864709426
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-02-12 10:41:18 +0300

    добавила selene==2.0.0rc9 в requirements.txt сделала Reformat code и Optimize imports

--- conftest.py
+++ conftest.py
@@ -1,9 +1,15 @@
 import pytest
-from selene import browser, be, have
+from selene import browser
+
 
 @pytest.fixture
 def browser_config():
     browser.config.window_height = 2500
     browser.config.window_width = 1400
+
+
+@pytest.fixture
+def browser_actions(browser_config):
+    browser.open('https://duckduckgo.com')
     yield
     browser.quit()
